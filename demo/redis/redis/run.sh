dcb76116f537d7ae18a9e3030ab8f6b3e697c786
chaosmonkey <windy3001@gmail.com>
2018-11-04 19:57:47 +0000

    Redis + sentinel kind of working. TODO: Get sentinels to talk to each other.

--- demo/redis/redis/run.sh
+++ demo/redis/redis/run.sh
@@ -3,9 +3,19 @@
 # VARS
 IP_ADDR=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 
-
+# setup redis.conf
 sed -i "s/%IP_ADDR%/${IP_ADDR}/" /redis/redis.conf
 
 
+# setup sentinel.conf
+sed -i "s/%IP_ADDR%/${IP_ADDR}/" /redis/sentinel.conf
+
+# Ideally, two seperate containers would be used for both the Redis
+# server and sentinel, but for the ease of simplicity I'll run them
+
+
 # run redis
-/usr/bin/redis-server /redis/redis.conf
+redis-server /redis/redis.conf
+
+# run sentinel
+redis-server /redis/sentinel.conf --sentinel
